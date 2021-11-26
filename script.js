ff45a7f979ee4cfebdc0248a25cc8e348a054b3a
Jason Hargrove <rijaha@gmail.com>
2021-11-25 21:13:27 -0600

    My first commit.

---
+++ script.js
@@ -0,0 +1,8 @@
+// 12 Important JavaScript Functions Every Web Developer Should Know
+
+// 1. Shorten the console log.
+
+const log = console.log.bind(document);
+log("does it work?");
+log("yes");
+log(5);
